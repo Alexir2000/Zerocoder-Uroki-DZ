96e77a44113343eeacc2488085350cd04c8385e1
Alexir2000 <hit2007@list.ru>
2024-06-18 18:10:06 +0300

    синхро

--- Urok42_VD06_Forms/uroki/Urok42_VD06_Forms/app/__init__.py
+++ Urok42_VD06_Forms/uroki/Urok42_VD06_Forms/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "yuo-will-never-guess"
+
+# Так как у нас проект находится не в корне,
+# то задаем путь к app через подпапки и  вместо слеша будет точка
 from uroki.Urok42_VD06_Forms.app import routes_dz
+
 # Прописываем путь в ини файле до app начиная от корня проекта через точку
 # Нельзя задавать имена папкам и файлам с тире - не будет работать!
 
--- Urok42_VD06_Forms/uroki/Urok42_VD06_Forms/app/routes_dz.py
+++ Urok42_VD06_Forms/uroki/Urok42_VD06_Forms/app/routes_dz.py
@@ -5,15 +5,20 @@ from markupsafe import Markup
 
 from uroki.Urok42_VD06_Forms.app import app
 
+# инициализируем будущий список анкет и задаем имя шаблону для страницы
 spisok_anket = []
 template1 = "anketa_dz.html"
 
-# Тут мы создаем свою функцию чтобы менять перенос строки на br
+# Тут мы создаем свою функцию чтобы менять перенос строки \n на <br>
+# Иначе мы вводим в поле хобби текст красивый с переводом строк,
+# а на выходе получаем все в одну строку -переносы строк исчезают - непорядок
 def nl2br(value):
     return Markup(value.replace("\n", "<br>"))
-# Тут мы создаем фильтр замены перевода строки на br - ПРИГОДИТСЯ!
+
+# А Тут мы создаем фильтр замены перевода строки на br - ПРИГОДИТСЯ!
 app.jinja_env.filters['nl2br'] = nl2br
 
+# Задаем реакцию в браузере на адреса страниц
 @app.route("/", methods=["GET", "POST"])
 def index():
 #использует метод POST, так как информация будет отправляться. Request method сравнивает данные с HTTP-запросом.
@@ -23,7 +28,8 @@ def index():
         gorod = request.form.get('gorod')
         hobby = request.form.get('hobby')
         vozrast = request.form.get('vozrast')
-        #создаёт условие для проверки наличия данных в полях title и content
+        # создаёт условие для проверки наличия данных в
+        # полях title и content и возраст. хобби не обязательное поле
         if name_user and gorod and vozrast:
             if not hobby:
                 hobby = 'Нет хобби'
@@ -33,6 +39,7 @@ def index():
                 'hobby': hobby,
                 'vozrast': vozrast
             }
+            # Добавляем анкету в список анкет
             spisok_anket.append(add_anketa)
             #использует для обновления страницы и предотвращения повторной отправки формы.
             return redirect(url_for('index'))
